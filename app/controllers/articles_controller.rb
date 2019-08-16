547e4d548740e0cf8bab1860526095e8c2126dd7
jaysonesmith <gh@nes.33mail.com>
2019-08-15 18:57:30 -0700

    initial navigation done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  def index
+      @articles = Article.all
+  end
+
   def new; end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
